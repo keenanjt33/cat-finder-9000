3ca8faf9936c63785633b9f404027b6ec65c3c8b
Keenan Tullis <keenanjt@umich.edu>
2021-04-23 19:33:34 -0400

    Footer update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,9 +5,17 @@ class Footer extends Component {
   render() {
     return (
       <div>
-        <footer className="bg-secondary">
-          <p className="max-w-xl p-5 sm:p-20 md:p-30">
-            <em>This is a work of fiction. Names, characters, places and incidents either are products of the author’s imagination or are used fictitiously. Any resemblance to actual events or locales or persons, living or dead, is entirely coincidental.</em>
+        <footer className="flex flex-wrap justify-around bg-secondary">
+          <p className="max-w-md p-5 sm:p-10">
+            <em>This site is for demonstration purposes only. Names and places are either are products of the author’s imagination or are used fictitiously. Any resemblance to actual locales, persons or cats, living or dead, is entirely coincidental.</em>
+          </p>
+          <p className="p-5 sm:p-10">
+            Copyright &copy;{' '}
+            {new Date().getFullYear()}
+            {' '}Keenan Tullis, All Rights Reserved
+            <br/>
+            <br/>
+            Source code can be found on <a className="underline" href="https://github.com/keenanjt33/cat-finder-9000">Github</a>
           </p>
         </footer>
       </div>
